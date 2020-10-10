91332832d387298a59f69f8478b24329d753c502
LinkWoong <zuiaineo@foxmail.com>
2020-10-09 22:12:42 -0400

    Add YouYube Comment Parser

---
+++ YoutubeCommentParser.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# Update the snippet metadata for a video. Sample usage:
+#   python update_video.py --video_id=<VIDEO_ID> --tags="<TAG1, TAG2>" --title="New title" --description="New description"
+
+import argparse
+import os
